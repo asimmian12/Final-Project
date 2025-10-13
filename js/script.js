6b52a0d78cb53a7b468460cef6113873cfbb7bdf
asim mian <asim.mian7878@gmail.com>
2025-10-13 01:56:50 +0100

    Need to fix Hamburger Menu

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// ------------------------TODO-------------------//
+function hamburgerMenu(){
+    document.getElementsByClassName("hamburger_menu");
+    if(document.getElementsByClassName("hamburger_menu") == "active"){
+        classList.add(".hamburger_menu");
+    } else{
+        classList.remove(".hamburger_menu");
+    }
+}
