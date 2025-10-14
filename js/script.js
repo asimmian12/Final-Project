eed74514736d6958648c65e496af45726acd1b74
asim mian <asim.mian7878@gmail.com>
2025-10-14 23:46:59 +0100

    fix: correct hamburger menu toggle logic and remove unnecessary code

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
 // ------------------------TODO-------------------//
 function hamburgerMenu(){
-<<<<<<< HEAD
     const hamburger_menu = document.querySelector(".hamburger_menu");
     if(hamburger_menu.classList.toggle(".hamburger_menu")){
         hamburger_menu.classList.add(".hamburger_menu");
     } else{
         hamburger_menu.classList.remove(".hamburger_menu");
-=======
-    document.getElementsByClassName("hamburger_menu");
-    if(document.getElementsByClassName("hamburger_menu") == "active"){
-        classList.add(".hamburger_menu");
-    } else{
-        classList.remove(".hamburger_menu");
->>>>>>> parent of d8d83bb (updated: I changed tailwind to normal css and html, as i found that easier and less time comsuming)
     }
     return hamburgerMenu
 }
