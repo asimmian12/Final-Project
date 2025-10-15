cadc12f773eaa7b5b5df604d30643fd53ee2d5ae
asim mian <asim.mian7878@gmail.com>
2025-10-15 03:17:03 +0100

    feat: implement hamburger menu and improve navbar structure across multiple pages

--- js/script.js
+++ js/script.js
@@ -8,14 +8,3 @@ function hamburgerMenu(){
     }
     return hamburgerMenu
 }
-
-// ------------------------TODO-------------------//
-// function hamburgerNav(){
-//     const hamburger_navbar = document.querySelector(".hamburger_navbar");
-//     if(hamburger_navbar.classList.toggle("active")){
-//         hamburger_navbar.classList.add("active")
-//     } else{
-//         hamburger_navbar.classList.add("active")
-//     }
-//     return hamburgerNav
-// }
