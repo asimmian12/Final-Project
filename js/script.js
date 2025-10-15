be9b79f18651288977e4ff4f6cf5b93d522165ec
asim mian <asim.mian7878@gmail.com>
2025-10-15 01:07:55 +0100

    fix: improve HTML structure and clean up footer links across multiple files

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // ------------------------TODO-------------------//
 function hamburgerMenu(){
     const hamburger_menu = document.querySelector(".hamburger_menu");
-    if(hamburger_menu.classList.toggle(".hamburger_menu")){
-        hamburger_menu.classList.add(".hamburger_menu");
+    if(hamburger_menu.classList.toggle("active")){
+        hamburger_menu.classList.add("active");
     } else{
-        hamburger_menu.classList.remove(".hamburger_menu");
+        hamburger_menu.classList.remove("active");
     }
     return hamburgerMenu
 }
