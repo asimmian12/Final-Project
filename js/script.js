cf58941cdb6f5658106a80b13cb4259291b2bc7d
asim mian <asim.mian7878@gmail.com>
2025-10-16 03:40:02 +0100

    refactor: rename button class from btn-primary to main_button for consistency across styles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 // Asim Mian
 // BSc Web and Mobile Development (Year 3)
 // 15/10/2025 
+
+
 // Try to futher fix Asim!!!!
+// Hamburger Menu
 function hamburgerMenu(){
     var hamburger_menu = document.querySelector(".div_navbar_vertical");
     if(hamburger_menu.classList.toggle("active")){
@@ -10,4 +13,12 @@ function hamburgerMenu(){
         hamburger_menu.classList.remove("active");
     }
     return hamburgerMenu;
+}
+
+// I made a function that can uws as character with spaces in mind, i'll modify this to only count uws
+function Paragraph_UWS(){
+    var textarea = document.querySelector(".textarea_uws_question");
+    var y = textarea.value.lastIndexOf("uws");
+    var result = document.querySelector(".resultDiv");
+    result.innerHTML = `Result: ${y}`;    
 }
