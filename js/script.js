2fcb89b0bfd13a293ab36719a86755c1e3197ebd
asim mian <asim.mian7878@gmail.com>
2025-10-16 01:00:25 +0100

    refactor: update comment to clarify intent for future fixes in script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 // Asim Mian
 // BSc Web and Mobile Development (Year 3)
 // 15/10/2025 
-
-// ------------------------TODO-------------------//
+// Try to futher fix Asim!!!!
 function hamburgerMenu(){
     var hamburger_menu = document.querySelector(".div_navbar_vertical");
     if(hamburger_menu.classList.toggle("active")){
