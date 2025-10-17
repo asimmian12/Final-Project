c98946df5aadf41ec2c74f2839259c756abb123d
asim mian <asim.mian7878@gmail.com>
2025-10-17 20:56:26 +0100

    refactor: update Paragraph_UWS function to search for 'uws' and improve comments for clarity

--- js/script.js
+++ js/script.js
@@ -14,17 +14,25 @@ function hamburgerMenu(){
     return hamburgerMenu;
 }
 
-// I made a function that can uws as character with spaces in mind, i'll modify this to only count uws
+// Orignal - I made a function that can uws as character with spaces in mind, i'll modify this to only count uws
+// function Paragraph_UWS(){
+//     let textarea = document.querySelector(".textarea_uws_question");
+//     let y = textarea.value.lastIndexOf("uws");
+//     let result = document.querySelector(".resultDiv");
+//     result.innerHTML = `Result: ${y}`;    
+// }
+
+// Modified to search through the string for checking conditions; if it has 'uws' anywhere in paragraph
 function Paragraph_UWS() {
     let textarea = document.querySelector(".textarea_uws_question");
-    let y = textarea.value.search(`uws`);
+    let y = textarea.value.search("uws");
     let result = document.querySelector(".resultDiv");
     result.innerHTML = `Result: ${y}`;
     for (let y = 0; y < textarea.value.length; y++) {
         if (y < textarea.value.length) {
             let score = textarea.value.substring(y, y + 3);
             console.log(score);
-            result.innerHTML = score;
+            score = result.innerHTML;
         }
     }
 }
