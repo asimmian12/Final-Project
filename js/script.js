fbfdfb8f079b6aa131cb3a9970e371d496dfbcf9
asim mian <asim.mian7878@gmail.com>
2025-10-21 00:28:12 +0100

    feat: implement cart functionality with add, clear, and remove buttons in task4.html and script.js, refine them futher Asim, please!!!

--- js/script.js
+++ js/script.js
@@ -4,28 +4,28 @@
 
 // Try to futher fix Asim!!!!
 // Hamburger Menu
-function hamburgerMenu(){
+function hamburgerMenu() {
     let hamburger_menu = document.querySelector(".div_navbar_vertical");
-    if(hamburger_menu.classList.toggle("active")){
+    if (hamburger_menu.classList.toggle("active")) {
         hamburger_menu.classList.add("active");
-    } else{
+    } else {
         hamburger_menu.classList.remove("active");
     }
     return hamburgerMenu;
 }
 // Need to fix this futher
-function addName(){
-    document.getElementById("main_button").onclick = function(){
-    let name = document.getElementById("name__Input").value;
-    let date = document.getElementById("date__input").value;
-    let income = document.getElementById("income__input").value;
-    let result = document.getElementById("paragraph_text");
-    result = result.innerHTML = `Added Persons:\n Name: ${name}, Date: ${date}, Amount: £${income}`;
-    result = localStorage.setItem(result);
-    result = localStorage.getItem(result);
-    console.log(result);
-    document.writeln(result);
-}
+function addName() {
+    document.getElementById("main_button").onclick = function () {
+        let name = document.getElementById("name__Input").value;
+        let date = document.getElementById("date__input").value;
+        let income = document.getElementById("income__input").value;
+        let result = document.getElementById("paragraph_text");
+        result = result.innerHTML = `Added Persons:\n Name: ${name}, Date: ${date}, Amount: £${income}`;
+        result = localStorage.setItem(`${name} ${date}`);
+        result = localStorage.getItem(`${name} ${date}`);
+        console.log(name, date);
+        document.writeln(name, date);
+    }
 }
 
 // Modified to use with a for loop to search through the string for checking conditions; if it has 'uws' anywhere in paragraph
@@ -42,3 +42,45 @@ function Paragraph_UWS() {
     //     }
     // }
 }
+// Add to Cart Btn
+function addCart() {
+    let add = document.getElementById("add_button")
+    let clear = document.getElementById("clear_button")
+    let remove = document.getElementById("subtract_button")
+    let paragraph_update = document.getElementById("paragraph-text");
+    let count = 0;
+ 
+
+    add.onclick = function(){
+        count = count + 1;
+        paragraph_update.innerHTML = `Items Added: ${count}`;
+    }
+    }
+
+// Clears Cart Btn
+function clearCart() {
+    let add = document.getElementById("add_button")
+    let clear = document.getElementById("clear_button")
+    let remove = document.getElementById("subtract_button")
+    let paragraph_update = document.getElementById("paragraph-text");
+    let count = 0;
+    
+    clear.onclick = function(){
+        count = count = 0;
+        paragraph_update.innerHTML = `Items Cleared: ${count}`;
+    }
+    }
+// Removes Cart Btn
+function removeCart() {
+    let add = document.getElementById("add_button")
+    let clear = document.getElementById("clear_button")
+    let remove = document.getElementById("subtract_button")
+    let paragraph_update = document.getElementById("paragraph-text");
+    let count = 0;
+    
+    remove.onclick = function(){
+        count = count - 1;
+        paragraph_update.innerHTML = `Items Removed: ${count}`;
+    }
+    }
+
