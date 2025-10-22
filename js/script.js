cf18d91a6b6d37a2ad38793566bca14c363e44c8
asim mian <asim.mian7878@gmail.com>
2025-10-22 05:04:55 +0100

    feat: enhance layout and styling in task2.html, task3.html, and task4.html; add ScrollReveal and RemixIcon for improved UI

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ function addToCart() {
     let add = document.querySelector(".add_button")
     let clear = document.querySelector(".clear_button")
     let remove = document.querySelector(".subtract_button")
-    let paragraph_update = document.querySelector(".paragraph-text");
+    let paragraph_update = document.querySelector(".paragraph_text");
     let count = 0;
 
     add.onclick = function () {
@@ -66,3 +66,113 @@ function addToCart() {
         paragraph_update.innerHTML = `Items Removed: ${count}`;
     }
 }
+
+// The js code can provide functionality to create a variable for ScrollReveal animations
+let scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// Animations with Swipper Libbary
+window.onload = function (){
+// The js code can provide functionality to reveal sections with ScrollReveal for .div_navbar_horizontal
+ScrollReveal().reveal(".div_navbar_horizontal", {
+  ...scrollRevealOption,
+});
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .div_layout
+ScrollReveal().reveal(".div_layout", {
+  ...scrollRevealOption,
+});
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .div_navbar_vertical
+ScrollReveal().reveal(".div_navbar_vertical", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .hamburger_menu
+ScrollReveal().reveal(".hamburger_menu", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .section_about_us
+ScrollReveal().reveal(".section_about_us", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .section_card
+ScrollReveal().reveal(".section_card", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .section_skills
+ScrollReveal().reveal(".section_skills", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for ..section__journey
+ScrollReveal().reveal(".section__journey", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .section_uws_question
+ScrollReveal().reveal(".section_uws_question", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .main_button
+ScrollReveal().reveal(".main_button", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .textarea_uws_question
+ScrollReveal().reveal(".textarea_uws_question", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .paragraph_text
+ScrollReveal().reveal(".paragraph_text", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .section__forms
+ScrollReveal().reveal(".section__forms", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .buttons
+ScrollReveal().reveal(".buttons", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .div__paragraph__results
+ScrollReveal().reveal(".div__paragraph__results", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .section_orders
+ScrollReveal().reveal(".section_orders", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .section_product_section_card
+ScrollReveal().reveal(".section_product_section_card", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .footer__paragraph__text
+ScrollReveal().reveal(".footer__paragraph__text", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .footer__paragraph__text__social__links
+ScrollReveal().reveal(".footer__paragraph__text__social__links", {
+  ...scrollRevealOption,
+})
+
+// The js code can provide functionality to reveal sections with ScrollReveal for .footer__paragraph__text__copyright
+ScrollReveal().reveal(".footer__paragraph__text__copyright", {
+  ...scrollRevealOption,
+})
+};
